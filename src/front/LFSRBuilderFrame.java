5b5ded9a30220fe2ce59c09a32a862fa548b988d
Оніщенко Даніїл <36864016+greentech72@users.noreply.github.com>
2022-05-07 09:26:50 +0300

    Fixed LFSRBuilderFrame

--- src/front/LFSRBuilderFrame.java
+++ src/front/LFSRBuilderFrame.java
@@ -2,18 +2,34 @@ package front;
 
 import logic.LFSR;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
+/**
+ * Linear feedback shift register builder frame.
+ * Draws scheme of linear feedback shift register
+ */
 public class LFSRBuilderFrame extends JFrame {
 
-    private LFSR lfsr;
-    private int seed;
+    /**
+     * Linear feedback shift register itself
+     */
+    private final LFSR lfsr;
+
+    /**
+     * Current state
+     */
     private int curr;
 
-    // UserInterface elemtns
+    /**
+     * UserInterface elements
+     */
     JLabel lCurr10, lCurr2;
     JTextField tfCurr10, tfCurr2;
     JButton bNext, bClose;
@@ -23,24 +39,30 @@ public class LFSRBuilderFrame extends JFrame {
      */
     private void setupUI() {
         setFont(LFSRFrame.font);
+
+        // create labels
         lCurr10 = new JLabel("Curr (10) : ");
         lCurr2 = new JLabel("Curr (2) : ");
+
+        // create fields
         tfCurr10 = new JTextField();
         tfCurr2 = new JTextField();
 
+        // set fonts
         lCurr10.setFont(LFSRFrame.font);
         lCurr2.setFont(LFSRFrame.font);
         tfCurr10.setFont(LFSRFrame.font);
         tfCurr2.setFont(LFSRFrame.font);
 
         // debug elements
-        JLabel[] debugs = new JLabel[12];
+        @SuppressWarnings("MismatchedReadAndWriteOfArray") JLabel[] debugs = new JLabel[12];
         for(JLabel label : debugs) {
             label = new JLabel("");
             label.setVisible(false);
             add(label);
         }
 
+        // add elements to window
         add(lCurr10);
         add(tfCurr10);
         add(lCurr2);
@@ -48,21 +70,111 @@ public class LFSRBuilderFrame extends JFrame {
     }
 
     /**
+     * Setups menu elements
+     */
+    private void setupMenu(){
+        // create menuBar
+        JMenuBar menuBar = new JMenuBar();
+
+        // create fileMenu
+        JMenu fileMenu = new JMenu("File");
+
+        // create fileMenu items
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem close = new JMenuItem("Close");
+
+        // add events
+        save.addActionListener(e -> {
+            // this is event to save current scheme to .png file
+            JFileChooser saveFile = new JFileChooser(); // create JFileChooser
+            saveFile.addChoosableFileFilter(new FileNameExtensionFilter("Only .png files", ".png"));
+            int saveDialogResult = saveFile.showSaveDialog(null); // show dialog
+            File fileToWriteTo = null;
+            switch (saveDialogResult) {
+                case JFileChooser.APPROVE_OPTION: // if user have chosen some file
+                    fileToWriteTo = saveFile.getSelectedFile(); // we get this file
+                    break;
+                case JFileChooser.ERROR_OPTION:
+                case JFileChooser.CANCEL_OPTION:
+                default:
+                    break;
+            }
+
+            if(fileToWriteTo != null) {
+                // create image 600x400
+                BufferedImage bufferedImage = new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB);
+                Graphics graphicsToDraw = bufferedImage.createGraphics();
+                graphicsToDraw.translate(0, -50); // translate graphics position so it can skip title and menu
+                paintAll(graphicsToDraw); // paint with help of local paint function
+                try {
+                    ImageIO.write(bufferedImage, "png", fileToWriteTo); // copy&paster from stackoverflow
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        });
+        close.addActionListener(e -> dispose());
+
+        // add elements to menus
+        fileMenu.add(save);
+        fileMenu.add(close);
+
+        // add menus to menuBar
+        menuBar.add(fileMenu);
+
+        // add menuBar to form
+        setJMenuBar(menuBar);
+
+        MouseListener myMouseListener = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                repaint();
+            }
+        };
+        menuBar.addMouseListener(myMouseListener);
+    }
+
+    /**
      * Setups buttons and buttons listeners
      */
     private void setupButtons() {
+        // create buttons
         bNext = new JButton("Next");
-        bNext.addActionListener(event -> {
-            curr = lfsr.next(curr);
-            repaint();
+        bClose = new JButton("Close");
 
+        // add action to buttons
+        bNext.addActionListener(event -> {
+            curr = lfsr.next(curr); // generate next state
+            repaint(); // repaint window
+
+            // set state
             tfCurr10.setText(String.valueOf(curr));
             tfCurr2.setText(String.format("%" + lfsr.getN() + "s", Integer.toBinaryString(curr)).replace(' ', '0'));
         });
-
-        bClose = new JButton("Close");
         bClose.addActionListener(event -> dispose());
 
+        // add buttons to form
         add(bNext);
         add(bClose);
     }
@@ -73,23 +185,22 @@ public class LFSRBuilderFrame extends JFrame {
      * @param lfsr linear forward shift register to work with
      * @param seed default init
      */
-    public LFSRBuilderFrame(LFSR lfsr, int seed) { // TODO Create BuilderState to divide business logic from front
+    public LFSRBuilderFrame(LFSR lfsr, int seed) {
+        // TODO Create BuilderState to divide business logic from front
         super();
 
         this.lfsr = lfsr;
-        this.seed = seed;
         this.curr = seed;
 
         setTitle("Tester");
         setResizable(false);
         setLayout(new GridLayout(9, 2));
-        setSize(600, 600);
+        setSize(600, 700);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 repaint();
-                System.out.println("Done"); // TODO Remove this
             }
 
             @Override
@@ -114,6 +225,8 @@ public class LFSRBuilderFrame extends JFrame {
         });
 
         setupUI();
+        setupMenu();
+
         tfCurr10.setText(String.valueOf(curr));
         tfCurr2.setText(String.format("%" + lfsr.getN() + "s", Integer.toBinaryString(curr)).replace(' ', '0'));
         setupButtons();
@@ -124,6 +237,8 @@ public class LFSRBuilderFrame extends JFrame {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+
+        g.translate(0, 50); // adds menuBar skip
 
         int WIDTH = 600;
         int HEIGHT = 400;
@@ -137,7 +252,7 @@ public class LFSRBuilderFrame extends JFrame {
 
         g.setColor(Color.black);
         g.drawString(String.valueOf(lfsr.outputOfSum(curr)), offset / 3 * 2, HEIGHT / 2); // show output of sum
-        g.drawLine(offset / 2, 355, 590, 355); // TODO Change to width height
+        g.drawLine(offset / 2, (int) (355 - 1.5 * offset), WIDTH - 10, (int) (HEIGHT - 1.5 * offset));
         g.drawString("out", WIDTH - offset * 3 / 2, HEIGHT - offset * 5 / 6);
 
         // draw triggers
@@ -149,7 +264,7 @@ public class LFSRBuilderFrame extends JFrame {
             g.drawRect(offset + (50 * j), HEIGHT - offset * 2, 30, 30);
         }
 
-        // draw sum
+        // draw sum and its lines
         g.drawRect(WIDTH - offset * 2, offset * 2, offset, HEIGHT - 4 * offset);
         g.drawLine(WIDTH - offset * 2 + offset, offset * 2 + (HEIGHT - 4 * offset) / 2,
                 WIDTH - offset * 2 + offset + 10, offset * 2 + (HEIGHT - 4 * offset) / 2);
@@ -160,7 +275,7 @@ public class LFSRBuilderFrame extends JFrame {
         g.drawLine(offset / 2, offset * 3 / 2,
                 offset / 2, HEIGHT - offset * 3 / 2);
 
-        // draw c
+        // draw c (lines)
         for(int i = 1; i < 11; i++) {
             int j = 10 - i;
             if(((lfsr.getC() >> j) & 1) == 1 &&
